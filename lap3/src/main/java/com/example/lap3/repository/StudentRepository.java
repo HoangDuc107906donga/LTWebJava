b707907a1caf4d7dbd47142d24b52177bca94d19
HoangDuc107906donga <leduc180606@gmail.com>
2026-08-22 10:44:38 +0700

    lab3

---
+++ lap3/src/main/java/com/example/lap3/repository/StudentRepository.java
@@ -0,0 +1,7 @@
+package com.example.lap3.repository;
+
+import com.example.lap3.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+}
